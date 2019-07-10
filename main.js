c69e9b5eaa8454c5f48e1c0398756d5357dd93b2
Radek <radoslawjakubiak@gmail.com>
2019-07-10 22:23:29 +0200

    still need to get the new tabs to work (js issues)

--- main.js
+++ main.js
@@ -18,5 +18,15 @@ function openCity(cityName) {
     x[i].style.display = "none";
   }
   document.getElementById(cityName).style.display = "block";
-}
+  console.log('main tab opened');
+};
+
+function openMenuItem(cityItem) {
+  var i;
+  var x = document.getElementsByClassName("city-item");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(cityItem).style.display = "block";
+};
 
