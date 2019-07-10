9adf47aae8a96be631789c62d58ba048228b8cd6
Radek <radoslawjakubiak@gmail.com>
2019-07-10 22:42:09 +0200

    solved the js issue - turns out I was editing the wrong js file all along!

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $(".col-1of9").click(function() {
+    this.toggleClass('col-active');
+  });
   console.log('ready!');
 });
 
@@ -9,5 +12,15 @@ function openCity(cityName) {
     x[i].style.display = "none";
   }
   document.getElementById(cityName).style.display = "block";
-}
+  console.log('main tab opened');
+};
+
+function openMenuItem(cityItem) {
+  var i;
+  var x = document.getElementsByClassName("city-item");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(cityItem).style.display = "block";
+};
 
