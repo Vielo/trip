fc12bd0caf055621b26f75558d14699b1f00297c
Radek <radoslawjakubiak@gmail.com>
2019-07-17 11:49:43 +0200

    Completed basic left menu styling, installed gallery files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
+  $(".col-1of9:first").addClass("col-active");
   $(".col-1of9").click(function() {
     $(".col-1of9").removeClass("col-active");
     $(this).toggleClass('col-active');
+  });
+  $(".city-item-menu-option:first").addClass("city-item-menu-option-active");
+  $(".city-item-menu-option").click(function() {
+    $(".city-item-menu-option").removeClass("city-item-menu-option-active");
+    $(this).toggleClass("city-item-menu-option-active");
   });
   console.log('ready!');
 });
