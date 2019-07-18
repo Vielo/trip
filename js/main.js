f9892a465ea8eff46e46e03effab63ef755a5da5
Radoslaw Jakubiak <radoslaw.jakubiak@experienceone.com>
2019-07-18 17:04:25 +0200

    dynamic loading of the left menu complete

--- js/main.js
+++ js/main.js
@@ -5,24 +5,22 @@ $(document).ready(function() {
     $(this).toggleClass('col-active');
     $(".city-item-menu-option").removeClass("city-item-menu-option-active");
     $(".city-item-menu-option:nth-child(1)").toggleClass("city-item-menu-option-active");
+    $(".city-item:nth-child(1)").attr('style','display: block');
   });
-  $(".city-item-menu-option:first").addClass("city-item-menu-option-active");
-  $(".city-item-menu-option").click(function() {
-    $(".city-item-menu-option").removeClass("city-item-menu-option-active");
+  $("body").on("click", ".city-item-menu-option", function(){
+    $('.city-item-menu-option').removeClass("city-item-menu-option-active");
     $(this).toggleClass("city-item-menu-option-active");
   });
+  $(".city-item-menu-option:first").addClass("city-item-menu-option-active");
   console.log('ready!');
 });
 
 
 function openCity(cityName) {
-  var i;
-  var x = document.getElementsByClassName("city");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(cityName).style.display = "block";
-  console.log('main tab opened');
+  loadNewCity(cityName);
+  $(".city-item-menu-option:first").addClass("city-item-menu-option-active");
+  console.log('new city loaded dynamically');
+
 };
 
 function openMenuItem(cityItem) {
@@ -30,9 +28,122 @@ function openMenuItem(cityItem) {
   var x = document.getElementsByClassName("city-item");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-  }
+  };
   document.getElementById(cityItem).style.display = "block";
 };
+
+/* controls the dynamic generation of the page content */
+
+function loadNewCity(cityName) {
+  let tempCityName = cityName;
+  $('#city-headline-name').html(pageContents[tempCityName].headlineData[0]);
+  $('#city-headline-date').html(pageContents[tempCityName].headlineData[1]);
+  console.log(pageContents[tempCityName].cityItems.length);
+  $('#item-menu').empty();
+  for (i = 0; i < pageContents[tempCityName].cityItems.length; i++) {
+    console.log('loop interation started');
+    $('#item-menu').append(`<div class="city-item-menu-option" onclick="openMenuItem('${cityName}-item${i+1}')"><div>${pageContents[cityName].cityItems[i].cityItemMenuOption}</div></div>`);
+    console.log('loop iteration complete')
+  };
+  loadNewCityItem(cityName);
+
+}
+
+function loadNewCityItem(cityItem) {
+
+
+}
+
+/*
+
+      <div id="city-item-container-wellington" class="city-items-container">
+          <div id="wellington-item1" class="city-item">
+              <div class="slideshow-container">
+                  <div class="mySlides fade">
+                      <div class="numbertext">1 / 6</div>
+                      <img src="img\idaho_springs\idaho_springs_1_max.jpg" style="width:100%">
+                  </div>
+                      
+                  <div class="mySlides fade">
+                      <div class="numbertext">2 / 6</div>
+                      <img src="img\idaho_springs\idaho_springs_2_max.jpg" style="width:100%">
+                  </div>
+                      
+                  <div class="mySlides fade">
+                      <div class="numbertext">3 / 6</div>
+                      <img src="img\idaho_springs\idaho_springs_3_max.jpg" style="width:100%">
+                  </div>
+                  <div class="mySlides fade">
+                      <div class="numbertext">4 / 6</div>
+                      <img src="img\idaho_springs\idaho_springs_4_max.jpg" style="width:100%">
+                  </div>
+                  <div class="mySlides fade">
+                      <div class="numbertext">5 / 6</div>
+                      <img src="img\idaho_springs\idaho_springs_5_max.jpg" style="width:100%">
+                  </div>
+                  <div class="mySlides fade">
+                      <div class="numbertext">6 / 6</div>
+                      <img src="img\idaho_springs\idaho_springs_6_max.jpg" style="width:100%">
+                  </div>
+                      
+                  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                  <div class="caption-container">
+                      <p id="caption"></p>
+                  </div>
+              
+                  <div class="row">
+                      <div class="column">
+                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_1_max.jpg" style="width:100%" onclick="currentSlide(1)" alt="Georgetown">
+                      </div>
+                      <div class="column">
+                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_2_max.jpg" style="width:100%" onclick="currentSlide(2)" alt="Georgetown">
+                      </div>
+                      <div class="column">
+                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_3_max.jpg" style="width:100%" onclick="currentSlide(3)" alt="Georgetown">
+                      </div>
+                      <div class="column">
+                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_4_max.jpg" style="width:100%" onclick="currentSlide(4)" alt="Georgetown">
+                      </div>
+                      <div class="column">
+                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_5_max.jpg" style="width:100%" onclick="currentSlide(5)" alt="Georgetown">
+                      </div>    
+                      <div class="column">
+                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_6_max.jpg" style="width:100%" onclick="currentSlide(6)" alt="Georgetown">
+                      </div>
+                  </div>    
+              </div>
+
+          </div>
+          <div id="wellington-item2" class="city-item" style='display:none'>
+              <p>Item 2</p>
+              <p>Should not be visible until menu item 2 is clicked</p>
+          </div>
+          <div id="wellington-item3" class="city-item" style='display:none'>
+              <p>Item 3</p>
+              <p>Should not be visible until menu item 2 is clicked</p>
+          </div>
+      </div>
+  </div>
+  
+</div>
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* The below controls how the gallery behaves */
 var slideIndex = 1;
---
+++ js/site-content.js
@@ -0,0 +1,309 @@
+var pageContents = {
+    denver1: {      /* CITY 1: DENVER (1) */
+        headlineData: ["Denver", "14.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ denv flight",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ denv first impressions",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ denv hotel",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    }, 
+    idaho_springs: {        /* CITY 2: IDAHO SPRINGS */
+        headlineData: ["Idaho Springs", "15.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ idaho option1",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ idaho option2",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ idaho option3",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+    wellington: {       /* CITY 3: WELLINGTON */
+        headlineData: ["Wellington", "16.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "Georgetown",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "Colorado National Monument",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "Wellington",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+    kanab: {        /* CITY 4: KANAB */
+        headlineData: ["Kanab", "17-18.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ kanab option1",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ kanab option2",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ kanab option3",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+    provo: {        /* CITY 5: PROVO */
+        headlineData: ["Provo", "19.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ provo option1",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ provo option2",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ provo option3",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+    pinedale: {        /* CITY 6: PINEDALE */
+        headlineData: ["Pinedale", "20.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ pin option1",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ pin option2",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ pin option3",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+    cody: {        /* CITY 7: CODY */
+        headlineData: ["Cody", "21-23.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ cody option1",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ cody option2",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ cody option3",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+    rock_springs: {        /* CITY 8: ROCK SPRINGS */
+        headlineData: ["Rock Springs", "24.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ rs option1",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ rs option2",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ rs option3",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+    denver2: {        /* CITY 9: DENVER */
+        headlineData: ["Denver", "25.06.2019"],
+        cityItems: [
+            {
+                cityItemMenuOption: "dyn_ den2 option1",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item1_Caption 1",
+                    img2: "item1_Caption 2",
+                    img3: "item1_Caption 3",
+                    img4: "item1_Caption 4",
+                    img5: "item1_Caption 5",
+                    img6: "item1_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ den2 option2",
+                showGallery: true,  /* decides whether to display the gallery for the city item */
+                images: {   /* controls the number of images passed to the gallery and determines their captions */
+                    img1: "item2_Caption 1",
+                    img2: "item2_Caption 2",
+                    img3: "item2_Caption 3",
+                    img4: "item2_Caption 4",
+                    img5: "item2_Caption 5",
+                    img6: "item2_Caption 6",
+                }
+            },
+            {
+                cityItemMenuOption: "dyn_ den2 option3",
+                showGallery: false,  /* decides whether to display the gallery for the city item */
+                contentText: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+            }
+        ]
+    },
+
+}
