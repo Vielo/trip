94c40bc94f0a9579eeb72502f96b3f7c5fa7a3ac
Radoslaw Jakubiak <radoslaw.jakubiak@experienceone.com>
2019-07-19 12:09:03 +0200

    moved thumbnails to the right side of the image gallery

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $(this).toggleClass('col-active');
     $(".city-item-menu-option").removeClass("city-item-menu-option-active");
     $(".city-item-menu-option:nth-child(1)").toggleClass("city-item-menu-option-active");
-    $(".city-item:nth-child(1)").attr('style','display: block');
+    $(".city-item:nth-child(1)").attr('style','display: flex');
   });
   $("body").on("click", ".city-item-menu-option", function(){
     $('.city-item-menu-option').removeClass("city-item-menu-option-active");
@@ -29,7 +29,7 @@ function openMenuItem(cityItem) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   };
-  document.getElementById(cityItem).style.display = "block";
+  document.getElementById(cityItem).style.display = "flex";
 };
 
 /* controls the dynamic generation of the page content */
@@ -53,95 +53,6 @@ function loadNewCityItem(cityItem) {
 
 
 }
-
-/*
-
-      <div id="city-item-container-wellington" class="city-items-container">
-          <div id="wellington-item1" class="city-item">
-              <div class="slideshow-container">
-                  <div class="mySlides fade">
-                      <div class="numbertext">1 / 6</div>
-                      <img src="img\idaho_springs\idaho_springs_1_max.jpg" style="width:100%">
-                  </div>
-                      
-                  <div class="mySlides fade">
-                      <div class="numbertext">2 / 6</div>
-                      <img src="img\idaho_springs\idaho_springs_2_max.jpg" style="width:100%">
-                  </div>
-                      
-                  <div class="mySlides fade">
-                      <div class="numbertext">3 / 6</div>
-                      <img src="img\idaho_springs\idaho_springs_3_max.jpg" style="width:100%">
-                  </div>
-                  <div class="mySlides fade">
-                      <div class="numbertext">4 / 6</div>
-                      <img src="img\idaho_springs\idaho_springs_4_max.jpg" style="width:100%">
-                  </div>
-                  <div class="mySlides fade">
-                      <div class="numbertext">5 / 6</div>
-                      <img src="img\idaho_springs\idaho_springs_5_max.jpg" style="width:100%">
-                  </div>
-                  <div class="mySlides fade">
-                      <div class="numbertext">6 / 6</div>
-                      <img src="img\idaho_springs\idaho_springs_6_max.jpg" style="width:100%">
-                  </div>
-                      
-                  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                  <div class="caption-container">
-                      <p id="caption"></p>
-                  </div>
-              
-                  <div class="row">
-                      <div class="column">
-                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_1_max.jpg" style="width:100%" onclick="currentSlide(1)" alt="Georgetown">
-                      </div>
-                      <div class="column">
-                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_2_max.jpg" style="width:100%" onclick="currentSlide(2)" alt="Georgetown">
-                      </div>
-                      <div class="column">
-                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_3_max.jpg" style="width:100%" onclick="currentSlide(3)" alt="Georgetown">
-                      </div>
-                      <div class="column">
-                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_4_max.jpg" style="width:100%" onclick="currentSlide(4)" alt="Georgetown">
-                      </div>
-                      <div class="column">
-                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_5_max.jpg" style="width:100%" onclick="currentSlide(5)" alt="Georgetown">
-                      </div>    
-                      <div class="column">
-                          <img class="demo cursor" src="img\idaho_springs\idaho_springs_6_max.jpg" style="width:100%" onclick="currentSlide(6)" alt="Georgetown">
-                      </div>
-                  </div>    
-              </div>
-
-          </div>
-          <div id="wellington-item2" class="city-item" style='display:none'>
-              <p>Item 2</p>
-              <p>Should not be visible until menu item 2 is clicked</p>
-          </div>
-          <div id="wellington-item3" class="city-item" style='display:none'>
-              <p>Item 3</p>
-              <p>Should not be visible until menu item 2 is clicked</p>
-          </div>
-      </div>
-  </div>
-  
-</div>
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
